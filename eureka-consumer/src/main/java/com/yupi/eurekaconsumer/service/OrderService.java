08ba7417128f5062ac41ef10a2e3cd4512556502
liyupi <592789970@qq.com>
2018-09-21 14:26:48 +0800

    update

--- eureka-consumer/src/main/java/com/yupi/eurekaconsumer/service/OrderService.java
+++ eureka-consumer/src/main/java/com/yupi/eurekaconsumer/service/OrderService.java
@@ -1,6 +1,5 @@
 package com.yupi.eurekaconsumer.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
